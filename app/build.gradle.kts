1e0f241e80936e54e22cedd82ccacb703e155aac
Kai <55243607+damagr@users.noreply.github.com>
2026-06-11 23:42:10 +0200

    Release v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pokechain"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.0.3-beta"
+        versionCode = 23
+        versionName = "1.0.3"
     }
 
     buildTypes {
